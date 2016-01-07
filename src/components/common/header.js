7a0ad7893a64ba8c05e3e523e4349eae3544af18
Dan Leatherman <dleatherman@gmail.com>
2016-01-07 12:36:25 -0500

    404 page

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -1,23 +1,19 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
 
 var Header = React.createClass({
   render: function() {
     return (
       <nav className="navbar navbar-default navbar-static-top">
         <div className="container">
-          <a className="navbar-brand" href="/#">React Pluralsight</a>
+          <Link to="app" className="navbar-brand">React Pluralsight</Link>
           <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a href="/#">Home</a>
-            </li>
-            <li>
-              <a href="/#about">About</a>
-            </li>
-            <li>
-              <a href="/#authors">Authors</a>
-            </li>
+            <li><Link to="app">Home</Link></li>
+            <li><Link to="about">About</Link></li>
+            <li><Link to="authors">Authors</Link></li>
           </ul>
         </div>
       </nav>
