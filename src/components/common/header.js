47e43135c47e7c50df60180e9b4a72ae94ebddb2
Dan Leatherman <dleatherman@gmail.com>
2016-01-06 20:44:51 -0500

    display author table in list

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -6,15 +6,20 @@ var Header = React.createClass({
   render: function() {
     return (
       <nav className="navbar navbar-default navbar-static-top">
-        <a className="navbar-brand" href="/#">React Pluralsight</a>
-        <ul className="nav navbar-nav">
-          <li>
-            <a href="/#">Home</a>
-          </li>
-          <li>
-            <a href="/#about">About</a>
-          </li>
-        </ul>
+        <div className="container">
+          <a className="navbar-brand" href="/#">React Pluralsight</a>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="/#">Home</a>
+            </li>
+            <li>
+              <a href="/#about">About</a>
+            </li>
+            <li>
+              <a href="/#authors">Authors</a>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
