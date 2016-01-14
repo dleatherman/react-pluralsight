be027e256bb793a422fb7c3bc75e554bc1f6c074
Dan Leatherman <dleatherman@gmail.com>
2016-01-14 15:29:00 -0500

    fully unidirectional data flow with flux

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -3,20 +3,27 @@
 var React = require('react');
 var AuthorApi = require('../../api/authorApi');
 var Link = require('react-router').Link;
-var AuthorApi = require('../../api/authorApi');
+var AuthorStore = require('../../stores/authorStore');
+var AuthorActions = require('../../actions/authorActions');
 var AuthorList = require('./authorList');
 
 var AuthorPage = React.createClass({
   getInitialState: function() {
     return {
-      authors: []
+      authors: AuthorStore.getAllAuthors()
     };
   },
 
-  componentDidMount: function() {
-    if (this.isMounted()) {
-      this.setState({ authors: AuthorApi.getAllAuthors() });
-    }
+  componentWillMount: function() {
+    AuthorStore.addChangeListener(this._onChange);
+  },
+
+  componentWillUnount: function() {
+    AuthorStore.removeChangeListener(this._onChange);
+  },
+
+  _onChange: function() {
+    this.setState({ authors: AuthorStore.getAllAuthors() });
   },
 
   render: function() {
